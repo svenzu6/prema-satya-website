32a7789e3467f0f6d16c9a7fb8076759d9c825b4
svenzu6 <zubcic.sven@gmail.com>
2021-03-04 12:15:37 +0100

    feature: skeleton

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>kurac</h1>
+    </div>
+  )
+}
