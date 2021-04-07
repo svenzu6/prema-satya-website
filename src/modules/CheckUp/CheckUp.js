f74ef3a36299b8ed389bf314fae740e58f587df1
svenzu6 <zubcic.sven@gmail.com>
2021-04-07 22:53:54 +0200

    fix(CheckUp): mobile layout

--- src/modules/CheckUp/CheckUp.js
+++ src/modules/CheckUp/CheckUp.js
@@ -2,7 +2,10 @@ import HeroTemplate from '../../components/TextTemplate/HeroTemplate'
 import ParagraphTemplate from '../../components/TextTemplate/ParagraphTemplate'
 import TitleTemplate from '../../components/TextTemplate/TitleTemplate'
 
-import { CheckUpContainer } from './CheckUp.styles'
+import {
+    CheckUpContainer,
+    CheckUpContent,
+} from './CheckUp.styles'
 
 export default function CheckUp() {
     return (
@@ -12,66 +15,90 @@ export default function CheckUp() {
                     ZAŠTO JE VAŽNA PRVA KONTROLA?
                 </TitleTemplate>
             </HeroTemplate>
-            <ParagraphTemplate>
-                Nakon uzimanja slučaja (prve homeopatske konzultacije) i propisivanja
-                prvog homeopatskog lijeka, od velike je važnosti  dogovaranje termina prve kontrole.
-                Idealan vremenski razmak su dva mjeseca.
-                <br />
-                <br />
-                Mnogima je to prilično dugo razdoblje za čekanje,
-                pogotovo ako nema nikakvih reakcija,
-                tako da se preporučuje razdoblje od jednog mjeseca nakon prvog uzimanja lijeka.
-                <br />
-                <br />
-                Zašto je važna prva kontrola?
-                <br />
-                Promjene, bilo pozitivne ili negativne, uglavnom se odviju tokom jednog mjeseca.
-                Homeopat tada na prvoj kontroli mora procijeniti klijentovo stanje prikupljajući
-                slijedeće informacije:
-                <br />
-                <br />
-                • Kakvo je opće stanje klijenta? Je li se zdravlje popravilo, pogoršalo ili je
-                ostalo nepromijenjeno nakon uzimanja lijeka?
-                <br />
-                <br />
-                • Energija ( ima li klijent ima više energije i motivacije ili je
-                energija smanjena ili nepromijenjena?)
-                <br />
-                <br />
-                • Glavne tjelesne poteškoće ( jesu li se dogodile promjene u glavnim tjelesnim problemima
-                zbog kojih je klijent i došao na homeopatsku konzultaciju? )
-                <br />
-                <br />
-                • Psihičko i emocionalno stanje ( je li na emocionalnoj i psihičkoj razini došlo do
-                promjena? Čak i najmanje promjene na ovim razinama pokazuju djelovanje lijeka. )
-                <br />
-                <br />
-                • Provjera starih simptoma ( homeopat prolazi kroz bilješke sa prve konzultacije i
-                provjerava simptom po simptom da bi ustanovio jesu li se promjene dogodile na bolje
-                ili gore ? )
-                <br />
-                <br />
-                • Novi simptomi ( jesu li se pojavili novi simptomi ili možda simptomi iz prošlosti? )
-                <br />
-                <br />
-                Na prvoj kontroli se uspostavlja bolji i otvoreniji odnos “klijent – homeopat”  što
-                omogućuje dublji uvid u samu „SRŽ“ slučaja.
-                <br />
-                <br />
-                Na kraju kontrole homeopat na osnovu  prethodnih informacija donosi određene
-                zaključke i odluke:
-                <br />
-                <br />
-                • Kakva je bila reakcija na prvi lijek , te
-                je li potrebno dati novi lijek ili je potrebno još čekati sa ponavljanjem
-                ili propisivanjem novog lijeka.
-                <br />
-                <br />
-                Bilo bi čak poželjno da klijenti sa kroničnim zdravstvenim problemima
-                vode kratke bilješke
-                redoslijedom što im se sve događalo od vremena kada su uzeli prvi puta homeopatski lijek što
-                bi olakšalo i njima i homeopatu vođenje prve kontrole.
-            </ParagraphTemplate>
+            <CheckUpContent>
+                <ParagraphTemplate>
+                    Nakon uzimanja slučaja (prve homeopatske konzultacije) i propisivanja
+                    prvog homeopatskog lijeka, od velike je važnosti  dogovaranje termina prve kontrole.
+                    Idealan vremenski razmak su dva mjeseca.
+                    <br />
+                    <br />
+                    Mnogima je to prilično dugo razdoblje za čekanje,
+                    pogotovo ako nema nikakvih reakcija,
+                    tako da se preporučuje razdoblje od jednog mjeseca nakon prvog uzimanja lijeka.
+                    <br />
+                    <br />
+                    Zašto je važna prva kontrola?
+                    <br />
+                    Promjene, bilo pozitivne ili negativne, uglavnom se odviju tokom jednog mjeseca.
+                    Homeopat tada na prvoj kontroli mora procijeniti klijentovo stanje prikupljajući
+                    slijedeće informacije:
+                    <br />
+                    <ul>
+                        <li>
+                            Kakvo je opće stanje klijenta? Je li se zdravlje popravilo, pogoršalo ili je
+                            ostalo nepromijenjeno nakon uzimanja lijeka?
+                        </li>
+                        <br />
+                        <br />
+                        <li>
+                            Energija (ima li klijent ima više energije i motivacije ili je
+                            energija smanjena ili nepromijenjena?)
+                        </li>
+                        <br />
+                        <br />
+                        <li>
+                            Glavne tjelesne poteškoće (jesu li se dogodile promjene u glavnim tjelesnim problemima
+                            zbog kojih je klijent i došao na homeopatsku konzultaciju? )
+                        </li>
+                        <br />
+                        <br />
+                        <li>
+                            Psihičko i emocionalno stanje (je li na emocionalnoj i psihičkoj razini
+                            došlo do
+                            promjena? Čak i najmanje promjene na ovim razinama
+                            pokazuju djelovanje lijeka.)
+                        </li>
+                        <br />
+                        <br />
+                        <li>
+                            Provjera starih simptoma (homeopat prolazi kroz bilješke sa prve konzultacije i
+                            provjerava simptome kako bi ustanovio jesu li
+                            nastale promjene bolje
+                            ili gore)
+                        </li>
+                        <br />
+                        <br />
+                        <li>
+                            Novi simptomi (jesu li se pojavili novi simptomi ili možda simptomi
+                            iz prošlosti?)
+                        </li>
+                    </ul>
+                    <br />
+                    Na prvoj kontroli se uspostavlja bolji i otvoreniji odnos “klijent – homeopat”  što
+                    omogućuje dublji uvid u samu „SRŽ“ slučaja.
+                    <br />
+                    <br />
+                    Na kraju kontrole homeopat na osnovu  prethodnih informacija donosi određene
+                    zaključke i odluke:
+                    <ul>
+                        <li>
+                            Kakva je reakcija na prvi lijek?
+                        </li>
+                        <li>
+                            Treba li promijeniti lijek?
+                        </li>
+                        <li>
+                            Treba li još čekati sa ponavljanjem
+                            ili propisivanjem novog lijeka?
+                        </li>
+                    </ul>
+                    <br />
+                    Preporuka za klijente sa kroničnim zdravstvenim problemima je da
+                    vode kratke bilješke
+                    redoslijedom događaja od vremena kada su prvi puta uzeli homeopatski lijek zbog
+                    realnijeg prikaza stanja na prvoj kontroli.
+                </ParagraphTemplate>
+            </CheckUpContent>
         </CheckUpContainer>
     )
 }
